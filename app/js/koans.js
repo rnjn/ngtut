4f682bc468cf21896e37d2685be896533e0df646
Ranjan Sakalley <runjan@gmail.com>
2014-09-01 11:55:57 +0530

    Basic

---
+++ app/js/koans.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var koans = function() {
+  var someIOCall = function() {
+
+    var url = 'http://api.duckduckgo.com/?q=DuckDuckGo&format=json';
+    var searchResults = {};
+    $.getJSON(url + '?callback=?', null, function(results) {
+      console.log('2');
+      searchResults = results;
+    });
+
+    return searchResults;
+  };
+
+  var makeIOCall = function() {
+    console.log('1');
+    var readText = someIOCall();
+    console.log('3');
+    return readText;
+  };
+
+  return {
+    makeIOCall: makeIOCall
+  };
+};
+
+koans();
---
+++ test/unit/koansSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('javascript koans', function() {
+  describe('callbacks vs linear code', function() {
+    it('you should know how to write callbacks', function() {
+      expect(koans().makeIOCall().Abstract).toNotBe(undefined);
+    });
+  });
+});
