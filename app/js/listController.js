065d87400a44a2928ba1165203cda6cf1dc6bb8d
Ranjan Sakalley <runjan@gmail.com>
2014-09-02 10:42:13 +0530

    Move from static templates to angular templates

--- app/js/listController.js
+++ app/js/listController.js
@@ -1,4 +1,23 @@
+'use strict';
 var listController = function($scope){
+  $scope.ultimateAnswerToLife = 42;
+  $scope.phones = [
+  {
+        'age': 0,
+        'id': 'motorola-xoom-with-wi-fi',
+        'imageUrl': 'img/phones/motorola-xoom-with-wi-fi.0.jpg',
+        'name': 'Motorola XOOM\u2122 with Wi-Fi',
+        'snippet': 'The Next, Next Generation\r\n\r\nExperience the future with Motorola XOOM with Wi-Fi, the worlds first tablet powered by Android 3.0 (Honeycomb).'
+    },
+    {
+        'age': 6,
+        'carrier': 'Best Buy',
+        'id': 'nexus-s',
+        'imageUrl': 'img/phones/nexus-s.0.jpg',
+        'name': 'Nexus S',
+        'snippet': 'Fast just got faster with Nexus S. A pure Google experience, Nexus S is the first phone to run Gingerbread (Android 2.3), the fastest version of Android yet.'
+    }
+  ];
 };
 
 ngtut.controller('ListController', ['$scope', listController]);
--- test/unit/listControllerSpec.js
+++ test/unit/listControllerSpec.js
@@ -11,7 +11,11 @@ describe('List controller', function() {
     });
   }));
 
-  it('should read static values on model', function() {
+  it('should read static primitives on model', function() {
     expect(scope.ultimateAnswerToLife).toBe(42);
+  });
+
+  it('should read static data on model', function(){
+    expect(scope.phones.length).toBe(2);
   });
 });
